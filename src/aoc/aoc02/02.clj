49d73a2b37b9ff2fe89c835b3f1469eeaab66413
Sean Moles <smoles@squarespace.com>
2022-12-02 18:31:16 -0500

    🎅🏻

--- src/aoc/aoc02/02.clj
+++ src/aoc/aoc02/02.clj
@@ -4,7 +4,7 @@
 
 (def 🪨📄✂️ (slurp "resources/aoc02input"))
 
-(def rps-map 
+(def rps-map
   {"A" :rock
    "B" :paper
    "C" :scissors})
@@ -14,39 +14,29 @@
    "Y" :draw
    "Z" :win})
 
-(def rps-points-map
-  {:rock 1
-   :paper 2
-   :scissors 3})
-
 (def outcome-points-map
   {:win 6
    :lose 0
    :draw 3})
 
+(def hands {:rock {:win :paper :lose :scissors :points 1}
+            :paper {:win :scissors :lose :rock :points 2}
+            :scissors {:win :rock :lose :paper :points 3}})
+
 (defn- choose-outcome [line]
   (let [[hand outcome] line]
     (cond (= outcome :draw) hand
-          (and (= hand :rock) (= outcome :win)) :paper
-          (and (= hand :rock) (= outcome :lose)) :scissors
-          (and (= hand :scissors) (= outcome :win)) :rock
-          (and (= hand :scissors) (= outcome :lose)) :paper
-          (and (= hand :paper) (= outcome :win)) :scissors
-          (and (= hand :paper) (= outcome :lose)) :rock)))
+          :else (get-in hands [hand outcome]))))
 
 (defn- map-code [code]
-  (let [codes (str/split code #" ")
-        hand (rps-map (first codes))
-        outcome (rps-outcome-map (second codes))]
-    (list hand outcome)))
-
-
+  (let [[hand outcome] (str/split code #" ")]
+    (list (rps-map hand) (rps-outcome-map outcome))))
 
 (defn- score-round [round]
   (let [outcome (second round)
         hand (choose-outcome round)]
     (+ (outcome-points-map outcome)
-       (rps-points-map hand))))
+       (get-in hands [hand :points]))))
 
 (defn score-all []
   (->> 🪨📄✂️
